1837b04bf6fee3446ce1b690ac7439bb34195928
Sevenpok <david20924col@gmail.com>
2022-03-11 01:01:11 -0600

    First Commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page</p>
+      <p>
+        My name is Gerber Colindres, i'm junior developer and create my first
+        website in next js
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const homePage = () => {
+  return (
+    <div>
+      <h1>Welcome to my api</h1>
+    </div>
+  );
+};
+
+export default homePage;
